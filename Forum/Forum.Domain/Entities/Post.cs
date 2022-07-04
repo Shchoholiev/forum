7cd419c7facdccc4430dcd5dbedfebab771bd0c4
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-07-04 15:13:41 +0300

    Added Thread and Post entities.

---
+++ Forum/Forum.Domain/Entities/Post.cs
@@ -0,0 +1,21 @@
+﻿using Forum.Domain.Common;
+using Forum.Domain.Entities.Identity;
+using MongoDB.Bson;
+
+namespace Forum.Domain.Entities
+{
+    public class Post : EntityBase
+    {
+        public ObjectId ThreadId { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime DatePosted { get; set; }
+
+        public int Likes { get; set; }
+
+        public User Author { get; set; }
+
+        public Post RepliedTo { get; set; }
+    }
+}
