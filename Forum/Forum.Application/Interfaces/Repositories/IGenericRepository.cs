9fa09b8d564218b30a1cf02f41984f420668c0f4
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-07-04 16:06:09 +0300

    Added interface for Generic Repository.

---
+++ Forum/Forum.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using EducationalPortal.Application.Paging;
+using Forum.Application.Paging;
+using Forum.Domain.Common;
+using MongoDB.Bson;
+using System.Linq.Expressions;
+
+namespace Forum.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : EntityBase
+    {
+        Task AddAsync(TEntity entity);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(ObjectId id);
+
+        Task<TEntity> GetOneAsync(ObjectId id);
+
+        Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters);
+
+        Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters, 
+                                              Expression<Func<TEntity, bool>> predicate);
+    }
+}
