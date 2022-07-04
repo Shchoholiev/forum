4218548046eb9bd34464aec133805c7b864c78d6
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-07-04 15:50:02 +0300

    Added MongoDbContext class.

---
+++ Forum/Forum.Application/MongoDB/MongoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Forum.Application.MongoDB
+{
+    public class MongoDbContext
+    {
+        private readonly MongoClient _client;
+
+        private readonly IMongoDatabase _db;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            this._client = new MongoClient(configuration.GetConnectionString("MongoDB"));
+            this._db = this._client.GetDatabase(configuration.GetValue<string>("MongoDatabaseName"));
+        }
+
+        public IMongoDatabase Db { get => this._db; }
+    }
+}
