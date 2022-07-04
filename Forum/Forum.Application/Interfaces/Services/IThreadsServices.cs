15c465d7960ff3cf51cf06bd3c6bd5bf767a353f
DESKTOP-J5SGACQ\rareb <sh.codework@gmail.com>
2022-07-04 16:17:29 +0300

    Added Threads Service.

---
+++ Forum/Forum.Application/Interfaces/Services/IThreadsServices.cs
@@ -0,0 +1,21 @@
+﻿using EducationalPortal.Application.Paging;
+using Forum.Application.Paging;
+using MongoDB.Bson;
+using System.Linq.Expressions;
+
+namespace Forum.Application.Interfaces.Services
+{
+    public interface IThreadsServices
+    {
+        Task AddAsync(Thread thread);
+
+        Task UpdateAsync(Thread thread);
+
+        Task DeleteAsync(ObjectId id);
+
+        Task<PagedList<Thread>> GetPageAsync(PageParameters pageParameters);
+
+        Task<PagedList<Thread>> GetPageAsync(PageParameters pageParameters,
+                                             Expression<Func<Thread, bool>> predicate);
+    }
+}
